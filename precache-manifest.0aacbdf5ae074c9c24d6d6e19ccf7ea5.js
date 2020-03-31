e86bfa8772366e5f78b24db94bb05cf36ad909c3
Grygorii Nakonechnyi <nakan88@gmail.com>
2020-03-31 16:08:37 +0300

    Updates

---
+++ precache-manifest.0aacbdf5ae074c9c24d6d6e19ccf7ea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c9ec5405df8c63e210b178b1bf6a94",
+    "url": "/json-view-react-app/index.html"
+  },
+  {
+    "revision": "4a385f5a190dcd067403",
+    "url": "/json-view-react-app/static/css/main.15cb3f62.chunk.css"
+  },
+  {
+    "revision": "9a38f50f2ec8cfb5d32a",
+    "url": "/json-view-react-app/static/js/2.1d9365b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/json-view-react-app/static/js/2.1d9365b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a385f5a190dcd067403",
+    "url": "/json-view-react-app/static/js/main.214d7518.chunk.js"
+  },
+  {
+    "revision": "a96427afaebc25b976b4",
+    "url": "/json-view-react-app/static/js/runtime-main.165ddac2.js"
+  }
+]);
